92dbff7eda5130fc2f20fb224a0fa7dafd5ffb7d
vigneshboddula <96077036+vigneshboddula@users.noreply.github.com>
2024-01-02 11:27:26 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Dew Drop Service!"
+    return "Hello, boddula vignesh _ 1BM20IS026"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
